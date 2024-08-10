5a6586ef53797cd45f4693d9320a563d14a81992
gsmarttek <chundusugyang@gmail.com>
2024-08-10 17:09:37 +0100

    Default layout and landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,7 @@
 <template>
-  <v-app-bar color="primary" prominent>
-    <v-app-bar-nav-icon
-      variant="text"
-      @click.stop="drawer = !drawer"
-    ></v-app-bar-nav-icon>
-
-    <v-toolbar-title>My files</v-toolbar-title>
-
-    <v-spacer></v-spacer>
-
-    <template v-if="$vuetify.display.mdAndUp">
-      <v-btn icon="mdi-magnify" variant="text"></v-btn>
-
-      <v-btn icon="mdi-filter" variant="text"></v-btn>
-    </template>
-
-    <v-btn icon="mdi-dots-vertical" variant="text"></v-btn>
-  </v-app-bar>
-
-  <v-navigation-drawer v-model="drawer" temporary>
-    <v-list :items="items"></v-list>
-  </v-navigation-drawer>
-
   <v-main style="height: 500px">
     <v-card-text>
-      The navigation drawer will appear from the bottom on smaller size screens.
+      <h1>This is the index or landing page of the application</h1>
     </v-card-text>
   </v-main>
 </template>
-
-<script>
-export default {
-  data: () => ({
-    drawer: false,
-    group: null,
-    items: [
-      {
-        title: "Foo",
-        value: "foo",
-      },
-      {
-        title: "Bar",
-        value: "bar",
-      },
-      {
-        title: "Fizz",
-        value: "fizz",
-      },
-      {
-        title: "Buzz",
-        value: "buzz",
-      },
-    ],
-  }),
-
-  watch: {
-    group() {
-      this.drawer = false;
-    },
-  },
-};
-</script>
