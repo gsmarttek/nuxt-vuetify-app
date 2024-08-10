342c9e3864d88ba2a9e0124662bdfe71916a9a9e
gsmarttek <chundusugyang@gmail.com>
2024-08-10 20:31:25 +0100

    Added head title in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,4 +27,13 @@ export default defineNuxtConfig({
       },
     },
   },
+  app: {
+    head: {
+      title: "My Nuxt App",
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1' }
+      ]
+    }
+  }
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
   <v-main style="height: 500px">
     <v-card-text>
-      <h1>This is the index or landing page of the application</h1>
+      <h1>Home Page</h1>
+      <p>This is the index or landing page of the application</p>
     </v-card-text>
   </v-main>
 </template>
