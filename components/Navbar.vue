ea27d7686e42fd993642ccbe2b3d1a66fa853473
gsmarttek <chundusugyang@gmail.com>
2024-08-18 01:24:09 +0100

    Add Navbar component

---
+++ components/Navbar.vue
@@ -0,0 +1,372 @@
+<template>
+  <v-app-bar color="primary" prominent>
+    <template v-if="!$vuetify.display.mdAndUp">
+      <v-app-bar-nav-icon
+        variant="text"
+        @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+    </template>
+
+    <template v-if="$vuetify.display.mdAndUp">
+      <NuxtLink to="/">
+        <v-avatar rounded="0" size="small" class="ml-4">
+          <v-img alt="John" src="/public/img/favicon-3.png"></v-img>
+        </v-avatar>
+      </NuxtLink>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-title style="text-align: center; font-size: 28px"
+        >OLA Hospital</v-toolbar-title
+      >
+    </template>
+
+    <v-spacer></v-spacer>
+
+    <v-btn @click="toggleTheme" icon>
+      <v-icon>{{ themeIcon }}</v-icon>
+    </v-btn>
+
+    <v-btn
+      variant="outlined"
+      to="/login"
+      size="small"
+      text="Login"
+      class="ml-3 mr-2"
+    >
+    </v-btn>
+
+    <v-btn icon="mdi-dots-vertical" variant="text"></v-btn>
+  </v-app-bar>
+
+  <template v-if="$vuetify.display.mdAndUp">
+    <v-app-bar height="50" flat color="#FFFFFF">
+      <v-spacer></v-spacer>
+      <v-menu transition="slide-x-transition">
+        <template v-slot:activator="{ props }">
+          <v-btn
+            variant="text"
+            :class="
+              theme.global.name.value === 'dark'
+                ? 'menu-dark-active'
+                : 'menu-light-active'
+            "
+            class="font-weght-medium"
+            v-bind="props"
+            >Home</v-btn
+          >
+        </template>
+      </v-menu>
+
+      <v-menu transition="slide-x-transition">
+        <template v-slot:activator="{ props }">
+          <v-btn
+            variant="text"
+            :color="menuColor"
+            class="font-weght-medium"
+            v-bind="props"
+            >Admin +</v-btn
+          >
+        </template>
+
+        <v-list>
+          <v-list-item>
+            <v-list-item-title class="mb-1">
+              <NuxtLink to="/" class="no-underline" :class="subMenuColor">
+                Admin Page 1
+              </NuxtLink>
+            </v-list-item-title>
+
+            <v-list-item-title class="mb-1">
+              <NuxtLink to="/" class="no-underline" :class="subMenuColor">
+                Admin Page 2
+              </NuxtLink>
+            </v-list-item-title>
+
+            <v-list-item-title class="mb-1">
+              <NuxtLink to="/" class="no-underline" :class="subMenuColor">
+                Admin Page 3
+              </NuxtLink>
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <v-menu transition="slide-x-transition">
+        <template v-slot:activator="{ props }">
+          <v-btn
+            variant="text"
+            :color="menuColor"
+            class="font-weght-medium"
+            v-bind="props"
+            >Patients +</v-btn
+          >
+        </template>
+
+        <v-list>
+          <v-list-item>
+            <v-list-item-title class="mb-1">
+              <NuxtLink to="/" class="no-underline" :class="subMenuColor">
+                Patients Page One
+              </NuxtLink>
+            </v-list-item-title>
+
+            <v-list-item-title class="mb-1">
+              <NuxtLink to="/" class="no-underline" :class="subMenuColor">
+                Patients Page Two
+              </NuxtLink>
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <v-menu transition="slide-x-transition">
+        <template v-slot:activator="{ props }">
+          <v-btn
+            variant="text"
+            :color="menuColor"
+            class="font-weght-medium"
+            v-bind="props"
+            >Insurance +</v-btn
+          >
+        </template>
+
+        <v-list>
+          <v-list-item>
+            <v-list-item-title class="mb-1">
+              <NuxtLink to="/" class="no-underline" :class="subMenuColor">
+                Insurance One
+              </NuxtLink>
+            </v-list-item-title>
+
+            <v-list-item-title class="mb-1">
+              <NuxtLink to="/" class="no-underline" :class="subMenuColor">
+                Insurance Two
+              </NuxtLink>
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <v-menu transition="slide-x-transition">
+        <template v-slot:activator="{ props }">
+          <v-btn
+            variant="text"
+            :color="menuColor"
+            class="font-weght-medium"
+            v-bind="props"
+            >Pharmacy &amp; Store +</v-btn
+          >
+        </template>
+
+        <v-list>
+          <v-list-item>
+            <v-list-item-title class="mb-1">
+              <NuxtLink to="/" class="no-underline" :class="subMenuColor">
+                Pharmacy Page One
+              </NuxtLink>
+            </v-list-item-title>
+
+            <v-list-item-title class="mb-1">
+              <NuxtLink to="/" class="no-underline" :class="subMenuColor">
+                Pharmacy Page Two
+              </NuxtLink>
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <v-menu transition="slide-x-transition">
+        <template v-slot:activator="{ props }">
+          <v-btn
+            variant="text"
+            :color="menuColor"
+            class="font-weght-medium"
+            v-bind="props"
+            >OPD +</v-btn
+          >
+        </template>
+
+        <v-list>
+          <v-list-item>
+            <v-list-item-title class="mb-1">
+              <NuxtLink to="/" class="no-underline" :class="subMenuColor">
+                OPD Page One
+              </NuxtLink>
+            </v-list-item-title>
+
+            <v-list-item-title class="mb-1">
+              <NuxtLink to="/" class="no-underline" :class="subMenuColor">
+                OPD Page Two
+              </NuxtLink>
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <v-menu transition="slide-x-transition">
+        <template v-slot:activator="{ props }">
+          <v-btn
+            variant="text"
+            :color="menuColor"
+            class="font-weght-medium"
+            v-bind="props"
+            >Registers +</v-btn
+          >
+        </template>
+
+        <v-list>
+          <v-list-item>
+            <v-list-item-title class="mb-1">
+              <NuxtLink to="/" class="no-underline" :class="subMenuColor">
+                Registers Page One
+              </NuxtLink>
+            </v-list-item-title>
+
+            <v-list-item-title class="mb-1">
+              <NuxtLink to="/" class="no-underline" :class="subMenuColor">
+                Registers Page Two
+              </NuxtLink>
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
+      <v-menu transition="slide-x-transition">
+        <template v-slot:activator="{ props }">
+          <v-btn
+            variant="text"
+            :color="menuColor"
+            class="font-weght-medium"
+            v-bind="props"
+            >Reports +</v-btn
+          >
+        </template>
+
+        <v-list>
+          <v-list-item>
+            <v-list-item-title class="mb-1">
+              <NuxtLink to="/" class="no-underline" :class="subMenuColor">
+                Reports Page One
+              </NuxtLink>
+            </v-list-item-title>
+
+            <v-list-item-title class="mb-1">
+              <NuxtLink to="/" class="no-underline" :class="subMenuColor">
+                Reports Page Two
+              </NuxtLink>
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+  </template>
+
+  <v-navigation-drawer v-model="drawer" temporary>
+    <h3 class="mt-4 ml-5" style="color: #6a87a3">
+      <NuxtLink to="/">
+        <v-avatar rounded="0" size="small" class="mb-4 mr-2">
+          <v-img alt="John" src="/public/img/favicon-3.png"></v-img>
+        </v-avatar>
+      </NuxtLink>
+      OLA Hospital
+    </h3>
+    <v-list>
+      <v-list-item prepend-icon="mdi-home" title="Home"></v-list-item>
+
+      <v-list-group value="Admin">
+        <template v-slot:activator="{ props }">
+          <v-list-item
+            v-bind="props"
+            prepend-icon="mdi-shield-account"
+            title="Admin"
+          ></v-list-item>
+        </template>
+
+        <v-list-item
+          v-for="([title, icon], i) in admins"
+          :key="i"
+          :title="title"
+          :value="title"
+        ></v-list-item>
+      </v-list-group>
+
+      <v-list-group value="Actions">
+        <template v-slot:activator="{ props }">
+          <v-list-item
+            v-bind="props"
+            prepend-icon="mdi-text-box-edit"
+            title="Actions"
+          ></v-list-item>
+        </template>
+
+        <v-list-item
+          v-for="([title, icon], i) in cruds"
+          :key="i"
+          :title="title"
+          :value="title"
+        ></v-list-item>
+      </v-list-group>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script setup>
+import { useTheme } from "vuetify";
+
+const drawer = ref(false);
+
+const admins = ref([
+  ["Management", "mdi-account-multiple-outline"],
+  ["Settings", "mdi-cog-outline"],
+]);
+
+const cruds = ref([
+  ["Create", "mdi-plus-outline"],
+  ["Read", "mdi-file-outline"],
+  ["Update", "mdi-update"],
+  ["Delete", "mdi-delete"],
+]);
+
+const theme = useTheme();
+
+const themeIcon = computed(() =>
+  theme.global.name.value === "dark" ? "mdi-weather-sunny" : "mdi-weather-night"
+);
+
+const menuColor = computed(() =>
+  theme.global.name.value === "dark" ? "" : "#3d5b99"
+);
+
+const subMenuColor = computed(() =>
+  theme.global.name.value === "dark" ? "menu-dark" : "menu-light"
+);
+
+const toggleTheme = () => {
+  theme.global.name.value =
+    theme.global.name.value === "dark" ? "light" : "dark";
+};
+</script>
+
+<style scoped>
+.no-underline {
+  text-decoration: none;
+}
+
+.menu-light {
+  color: #3d5b99;
+}
+
+.menu-dark {
+  color: #ffffff;
+}
+
+.menu-light-active {
+  color: #fe0000;
+}
+
+.menu-dark-active {
+  color: grey;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -10,72 +10,109 @@
               :src="item.src"
               cover
             >
-              <v-row>
+              <!-- <v-row>
                 <v-col cols="12" sm="6">
                   <div class="d-flex fill-height justify-center align-center">
-                    <h2 class="text-white top">{{ item.title }}</h2>
+                    <h2 class="text-black top">{{ item.title }}</h2>
                     <br />
                   </div>
                   <div class="d-flex fill-height justify-center align-center">
-                    <p class="text text-white">{{ item.text }}</p>
+                    <p class="text text-black">{{ item.text }}</p>
                   </div>
                   <div class="d-flex fill-height justify-center align-center">
-                    <v-btn style="margin-top: -120px">Buy Now</v-btn>
+                    <v-btn style="margin-top: -120px" to="/login">Login</v-btn>
                   </div>
                 </v-col>
-              </v-row>
+              </v-row> -->
             </v-carousel-item>
           </v-carousel>
         </v-col>
       </v-row>
 
-      <v-card-text>
-        <h1>Home Page</h1>
-        <p>This is the index or landing page of the application</p>
+      <v-card-text class="mt-10 mb-15 text-center">
+        <h1 :class="themeH1" style="font-size: 2em">
+          Welcome To OLA Hospital App
+        </h1>
+        <div class="d-flex justify-center">
+          <p style="width: 70%; color: #777">
+            This is the home page of the OLA Hospital Application, Please select
+            an item from the main menu above or click on the Patient
+            Information, Laboratory Results, and Pharmacy &amp; Drugs links
+            bellow for more access.
+          </p>
+        </div>
       </v-card-text>
     </v-container>
   </v-main>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      items: [
-        {
-          src: "img/carousel/7.jpg",
-          title: "Surface Studio",
-          text: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès ",
-        },
-        {
-          src: "img/carousel/1.jpg",
-          title: "Surface Studio",
-          text: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès ",
-        },
-        {
-          src: "img/carousel/2.jpg",
-          title: "Surface Studio",
-          text: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès ",
-        },
-        {
-          src: "img/carousel/3.jpg",
-          title: "Surface Studio",
-          text: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès ",
-        },
-        {
-          src: "img/carousel/4.jpg",
-          title: "Surface Studio",
-          text: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès ",
-        },
-        {
-          src: "img/carousel/5.jpg",
-          title: "Surface Studio",
-          text: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès ",
-        },
-      ],
-    };
+<script setup>
+import { useTheme } from "vuetify";
+
+const theme = useTheme();
+
+const themeH1 = computed(() =>
+  theme.global.name.value === "dark" ? "h1-dark-color" : "h1-light-color"
+);
+
+const items = ref([
+  {
+    src: "img/carousel/1.jpg",
+    title: "Surface Studio",
+    text: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès ",
   },
-};
+  {
+    src: "img/carousel/2.jpg",
+    title: "Surface Studio",
+    text: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès ",
+  },
+  {
+    src: "img/carousel/3.jpg",
+    title: "Surface Studio",
+    text: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès ",
+  },
+  {
+    src: "img/carousel/4.jpg",
+    title: "Surface Studio",
+    text: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès ",
+  },
+  {
+    src: "img/carousel/5.jpg",
+    title: "Surface Studio",
+    text: "Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès ",
+  },
+]);
+
+const populars = ref([
+  {
+    image: "img/populars/9.jpg",
+    title: " Enjoy The Beauty Place in Greece City",
+    subtitle1: "Greece",
+    subtitle2: "4 Day's 3 Night",
+    money: "$1200",
+  },
+  {
+    image: "img/populars/10.jpg",
+    title: " Enjoy The Beauty Place in Maldivs Beach",
+    subtitle1: "Maldivs",
+    subtitle2: "5 Day's 4 Night",
+    money: "$1300",
+  },
+  {
+    image: "img/populars/11.jpg",
+    title: " Enjoy The Beauty Place in Bhutan City",
+    subtitle1: "Bhutan",
+    subtitle2: "3 Day's 2 Night",
+    money: "$900",
+  },
+  {
+    image: "img/populars/12.jpg",
+    title: " Enjoy The Beauty Place in Paris City",
+    subtitle1: "Paris",
+    subtitle2: "4 Day's 3 Night",
+    money: "$1700",
+  },
+]);
 </script>
 
 <style scoped>
@@ -88,5 +125,11 @@ export default {
 }
 .top-day {
   margin-top: 120px;
+}
+.h1-light-color {
+  color: #566fa0;
+}
+.h1-dark-color {
+  color: #dce0e7;
 }
 </style>
